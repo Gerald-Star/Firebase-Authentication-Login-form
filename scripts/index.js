0a359465e553fa01dc910fcc8e15e015e907d896
Gerald-Star <chimagerald3@gmail.com>
2024-03-20 01:17:06 +0100

    configure firebase authentication and create login and register form

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+//DOM 
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function () {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
+});
